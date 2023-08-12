c8d21d434c1e450c11a5adce4f41b11f28ea3c0a
AlexLenk <alexlenk.ru@gmail.com>
2023-08-12 07:04:35 +0300

    Refused regular expressions and JSON.stringify. Fixed the problem with the fs.readdirSync function.

--- index.js
+++ index.js
@@ -16,7 +16,9 @@ function extractPages(directoryGlob) {
     return [];
   }
 
-  const files = fs.readdirSync(directory).map(file => path.join(directory, file)).sort();
+  const allEntities = fs.readdirSync(directory).map(entity => path.join(directory, entity));
+
+  const files = allEntities.filter(entity => fs.statSync(entity).isFile()).sort();
 
   if (!files.length) {
     console.error(`No files found with the pattern: ${ directoryGlob }`);
@@ -37,17 +39,33 @@ function createPagesStructure(files, template) {
   return pagesList;
 }
 
+function objectToString(obj) {
+  let str = '{\n';
+
+  for (let key in obj) {
+    str += `  '${ key }': {\n`;
+    for (let innerKey in obj[key]) {
+      str += `    ${ innerKey }: ${ arrayToString(obj[key][innerKey]) },\n`;
+    }
+    str += '  },\n';
+  }
+
+  str += '};\n';
+
+  return str;
+}
+
+function arrayToString(arr) {
+  return '[' + arr.map(item => `'${ item }'`).join(', ') + ']';
+}
+
 function saveResults(pagesList, outputDirectory, outputFileName) {
   if (!fs.existsSync(outputDirectory)) {
     fs.mkdirSync(outputDirectory, { recursive: true });
   }
 
   const outputPath = path.join(outputDirectory, outputFileName);
-  const outputContent = `module.exports = ${ JSON.stringify(pagesList, null, 2)
-    .replace(/"/g, '\'')
-    .replace(/(\[\s+'[^']+'\s+])/g, (match, p1) => p1.replace(/\s+/g, ''))
-    .replace(/'([a-zA-Z0-9_]+)':/g, '$1:')
-  };\n`;
+  const outputContent = `module.exports = ${ objectToString(pagesList) }`;
 
   fs.writeFileSync(outputPath, outputContent, 'utf8');
   console.log(`Scanned and saved file data to ${ outputPath }`);
